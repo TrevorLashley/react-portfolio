8737119613c1fb79f237cabd028f890efb8220cc
Trevor Lashley <trevor.lashley@hotmail.ca>
2024-03-15 22:36:53 -0400

    added styling to navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,11 +1,50 @@
+import { NavLink } from "react-router-dom";
+
+import styled from "styled-components";
 
 const NavBar = () => {
+  return (
+    <StyledNav>
+      <div>
+        <StyledNavButton className="StyledNavButton" to="/">
+          Home
+        </StyledNavButton>
+        <StyledNavButton className="StyledNavButton" to="/users">
+         About
+        </StyledNavButton>
+        <StyledNavButton className="StyledNavButton" to="/">
+          Projects
+        </StyledNavButton>
+        <StyledNavButton className="StyledNavButton" to="/users">
+         Contact me!
+        </StyledNavButton>
 
-   return (
+      </div>
+    </StyledNav>
+  );
+};
 
-    <>NavBar</>
+export default NavBar;
 
-   ) 
-}
+const StyledNav = styled.nav`
 
-export default NavBar
+width: 100%;
+
+padding: 10px;
+
+
+`
+
+const StyledNavButton = styled.button`
+
+   margin-left: 20px;
+   font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol";
+   text-decoration: none;
+   font-size: 1.2em;
+   font-weight: bold;
+   background-color: transparent;
+   color: inherit;
+   border-style: none;
+   &:hover{color: gray}
+  
+`;
